a371625814f371c0d063729f3328f3a5066e2809
Jeff MacInnes <jeff.macinnes@gmail.com>
2023-01-05 14:16:43 -0800

    updated deployToGithub script

--- scripts/deployToGithub.sh
+++ scripts/deployToGithub.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# make sure github repo is set to host pages from docs folder
+# and svelte.config.js has 
+#		adapter: adapter({
+#			pages: 'docs',
+#			assets: 'docs'
+#		}),
 npm run build
 git add *
 git commit -m "deploying to github pages"
